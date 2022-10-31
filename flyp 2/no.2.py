db2263f609ddf8321e8208e275427d1524429e27
padil alamsyah <202210715294@mhs.ubharajaya.ac.id>
2022-10-31 17:01:48 +0700

    tugas2 sudah selesai
    kirim

---
+++ flyp 2/no.2.py
@@ -0,0 +1,5 @@
+# pencabangan IF dengan 1 kasus
+nama =input("Masukkan Nama : ")
+if nama == "fadhil alamsyah" :
+    print("hallo", nama, "welcome")
+    print()
---
+++ flyp 4/no.4.py
@@ -0,0 +1,18 @@
+# pengulangan pakai for
+for i in range(7):
+    print("pengulangan ke - ", i)
+
+print("\n")
+
+listMobil = ["Avanza", "Bmw", "Ducati", "Xenia", "Mercy"]
+for Mobil in listMobil :
+    print(Mobil)
+
+print("\n")
+
+# pengulangan pakai while
+angka = 1
+
+while angka > 10 :
+    angka += 1
+    print("contoh penggunaan sintax while")
